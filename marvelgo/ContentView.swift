4056556936ef7aef8d011eddbb65705aac58e0bb
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-06-12 21:04:30 -0300

    Initial Commit

---
+++ marvelgo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  marvelgo
+//
+//  Created by Antonio Vuono on 12/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ marvelgo/marvelgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvelgoApp.swift
+//  marvelgo
+//
+//  Created by Antonio Vuono on 12/06/22.
+//
+
+import SwiftUI
+
+@main
+struct marvelgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
